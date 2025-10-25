da6323f50f5d5336771912f33c25af1c210805ca
volta2030 <volta2030@gmail.com>
2025-10-25 11:56:38 +0900

    feat: implement pad function #88

--- renderer/main_renderer.js
+++ renderer/main_renderer.js
@@ -206,6 +206,7 @@ ipcRenderer.on("colorpickerImgCMD", async (event, res) => {
 ipcRenderer.on("padImgCMD", async (event, res) => {
   const currentLayer = imageLayerQueue[imgKitRenderer.currentIndex];
   if (!currentLayer) return;
+  currentLayer.processImage({ extend: { top: 1, bottom: 1, left: 1, right: 1 } });
 });
 
 
